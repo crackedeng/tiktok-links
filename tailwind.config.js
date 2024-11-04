795f3db21aa90318b212637a4161954b44386b89
crackedeng <tochey@outlook.com>
2024-11-03 20:28:49 -0500

    v1 progress

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+  },
+  plugins: [],
+}
